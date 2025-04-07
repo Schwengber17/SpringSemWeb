c2f222c1c5c539481a6755ffeabc3e7e07507ef1
Schwengber17 <gabriel.schwengber@edu.pucrs.br>
2025-04-07 14:50:45 -0300

    Finalizando temporariamente

--- src/main/java/br/com/alura/screenmatch/Principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/Principal/Principal.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collector;
@@ -30,15 +33,25 @@ public class Principal {
     private ConverterDados converter = new ConverterDados();
     List<DadosTemporada> listaTemporadas = new ArrayList<>();
 
-    public void ExibeMenu() {
+    public int ExibeMenu() {
 
         System.out.println("Bem vindo ao Menu de Filmes e Series");
         System.out.println("1 - Filmes");
         System.out.println("2 - Series");
         System.out.println("3 - Sair");
-
+        System.out.println("Escolha uma opção:");
+        int opcao = sc.nextInt();
+        sc.nextLine();
+        if (opcao < 1 || opcao > 3) {
+            System.out.println("Opção inválida. Tente novamente.");
+            ExibeMenu();
+        } else {
+            System.out.println("Você escolheu a opção: " + opcao);
+        }
+        return opcao;
     }
 
+    // Método possui gargalo na saída do sistema
     public List<DadosTemporada> criaListaTemporadas(DadosSerie dadosSerie) {
         for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
             var jsonTemporada = consumoApi
@@ -47,6 +60,32 @@ public class Principal {
             listaTemporadas.add(dadosTemporada);
         }
         return listaTemporadas;
+    }
+
+    // Criar metodo para criar lista de episodios
+
+    public void mediaAvaliacao(DadosSerie serie) {
+        List<Episodeo> episodeos = listaTemporadas.stream()
+                .flatMap(t -> t.episodeos().stream()
+                        .map(e -> new Episodeo(t.numero(), e)))
+                .collect(Collectors.toList());
+        System.out.println("Média de avaliação por temporada: ");
+        Map<Integer, String> avaliacoesTemp = episodeos.stream()
+                .filter(e -> e.getAvaliacao() != null && e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodeo::getTemporada,
+                        Collectors.averagingDouble(Episodeo::getAvaliacao)))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        entry -> String.format("%.1f", entry.getValue())));
+        System.out.println(avaliacoesTemp);
+        // Por Estatistica do Spring
+        DoubleSummaryStatistics est = episodeos.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodeo::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
     }
 
     public void verTemporadas(DadosSerie dadosSerie) {
@@ -110,44 +149,44 @@ public class Principal {
         var jsonSerie = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSerie dadosSerie = converter.obterDados(jsonSerie, DadosSerie.class);
         System.out.println(dadosSerie);
+        if (listaTemporadas.size() != 0) {
+            listaTemporadas.clear();
+            criaListaTemporadas(dadosSerie);
+        } else {
+            criaListaTemporadas(dadosSerie);
+        }
         System.out.println("1 - Ver temporadas");
         System.out.println("2 - Ver episódios");
         System.out.println("3 - Ver episódios por ano");
         System.out.println("4 - Achar dados do episódio por nome ");
-        System.out.println("5 - Sair");
-        int opcao = sc.nextInt();
+        System.out.println("5 - Média de avaliação por temporada");
+        System.out.println("6 - Sair");
+        Integer opcao = sc.nextInt();
         sc.nextLine();
         switch (opcao) {
             case 1:
                 verTemporadas(dadosSerie);
                 break;
             case 2:
-                if (listaTemporadas.isEmpty()) {
-                    criaListaTemporadas(dadosSerie);
-                } else {
-                    verEpisodeosDasTemporadas(dadosSerie);
-                }
+                verEpisodeosDasTemporadas(dadosSerie);
                 break;
             case 3:
                 // Em vez de pedir o ano aqui, pedir no método
                 System.out.println("Qual ano?");
                 var ano = sc.nextInt();
                 sc.nextLine();
-                if (listaTemporadas.isEmpty()) {
-                    criaListaTemporadas(dadosSerie);
-                }
                 verEpAfterYear(dadosSerie, ano);
                 break;
             case 4:
                 // Em vez de pedir o nome aqui, pedir no método
                 System.out.println("Qual o nome do episódio?");
                 var nomeEpisodio = sc.nextLine();
-                if (listaTemporadas.isEmpty()) {
-                    criaListaTemporadas(dadosSerie);
-                }
                 DadosEpNome(dadosSerie, nomeEpisodio);
                 break;
             case 5:
+                mediaAvaliacao(dadosSerie);
+                break;
+            case 6:
                 System.out.println("Saindo...");
                 break;
             default:
@@ -160,11 +199,9 @@ public class Principal {
         boolean continuar = true;
 
         while (continuar) {
-            ExibeMenu();
-            System.out.println("Escolha uma opção:");
-            int opcao = sc.nextInt();
-            sc.nextLine(); // Limpa o buffer do scanner
-            switch (opcao) {
+            int opcao2 = ExibeMenu();
+
+            switch (opcao2) {
                 case 1:
                     MenuOpcoesFilmes();
                     break;
@@ -176,7 +213,7 @@ public class Principal {
                     continuar = false;
                     break;
                 default:
-                    throw new IllegalArgumentException("Opção inválida: " + opcao);
+                    throw new IllegalArgumentException("Opção inválida: " + opcao2);
             }
         }
     }
